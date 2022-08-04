17dc26ed4732e6ecbbaac56f552ce9d012b2ecb9
nkere30 <ninokereselidze30@gmail.com>
2022-08-04 06:03:45 +0400

    Completed

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int vairable = 10;
+        int variable = 10;
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
